99652f75951229133ce38de68a17322d9f0c3c4f
FidyBack <abelcan@al.insper.edu.br>
2019-03-08 19:19:52 -0300

    Implementação Hexadecimal 3 casas

--- Projetos/C-LogicaCombinacional/src/TopLevel.vhd
+++ Projetos/C-LogicaCombinacional/src/TopLevel.vhd
@@ -22,26 +22,75 @@ entity TopLevel is
 		SW      : in  std_logic_vector(9 downto 0);
 		HEX0    : out std_logic_vector(6 downto 0); -- 7seg0
 		HEX1    : out std_logic_vector(6 downto 0); -- 7seg1
-		HEX2    : out std_logic_vector(6 downto 0); -- 7seg2
-		HEX3    : out std_logic_vector(6 downto 0); -- 7seg3
-		LEDR    : out std_logic_vector(9 downto 0)
+		HEX2    : out std_logic_vector(6 downto 0) -- 7seg2
 	);
+	
 end entity;
 
 ----------------------------
--- Implementacao do bloco -- 
+-- Implementacao do bloco --
 ----------------------------
 architecture rtl of TopLevel is
+constant none : std_logic_vector(6 downto 0) := ('1','1','1','1','1','1','1');
+constant n0 : std_logic_vector(6 downto 0) := ('1','0','0','0','0','0','0');
+constant n1 : std_logic_vector(6 downto 0) := ('1','1','1','1','0','0','1');
+constant n2 : std_logic_vector(6 downto 0) := ('0','1','0','0','1','0','0');
+constant n3 : std_logic_vector(6 downto 0) := ('0','1','1','0','0','0','0');
+constant n4 : std_logic_vector(6 downto 0) := ('0','0','1','1','0','0','1');
+constant n5 : std_logic_vector(6 downto 0) := ('0','0','1','0','0','1','0');
+constant n6 : std_logic_vector(6 downto 0) := ('0','0','0','0','0','1','0');
+constant n7 : std_logic_vector(6 downto 0) := ('1','1','1','1','0','0','0');
+constant n8 : std_logic_vector(6 downto 0) := ('0','0','0','0','0','0','0');
+constant n9 : std_logic_vector(6 downto 0) := ('0','0','1','0','0','0','0');
+constant a : std_logic_vector(6 downto 0) := ('0','0','0','1','0','0','0');
+constant b : std_logic_vector(6 downto 0) := ('0','0','0','0','0','1','1');
+constant c : std_logic_vector(6 downto 0) := ('1','0','0','0','1','1','0');
+constant d : std_logic_vector(6 downto 0) := ('0','1','0','0','0','0','1');
+constant e : std_logic_vector(6 downto 0) := ('0','0','0','0','1','1','0');
+constant f : std_logic_vector(6 downto 0) := ('0','0','0','1','1','1','0');
 
---------------
--- signals
---------------
-
----------------
--- implementacao
----------------
 begin
-	HEX0(1) <=SW(0);
-	HEX0(4) <=SW(0);
-	LEDR(0) <= SW(0);
+	
+	HEX0 <= 
+		n0 when SW(0) = '0' and SW(1) = '0' and SW(2) = '0' and SW(3) = '0' else
+		n1 when SW(0) = '1' and SW(1) = '0' and SW(2) = '0' and SW(3) = '0' else
+		n2 when SW(0) = '0' and SW(1) = '1' and SW(2) = '0' and SW(3) = '0' else
+		n3 when SW(0) = '1' and SW(1) = '1' and SW(2) = '0' and SW(3) = '0' else
+		n4 when SW(0) = '0' and SW(1) = '0' and SW(2) = '1' and SW(3) = '0' else
+		n5 when SW(0) = '1' and SW(1) = '0' and SW(2) = '1' and SW(3) = '0' else
+		n6 when SW(0) = '0' and SW(1) = '1' and SW(2) = '1' and SW(3) = '0' else
+		n7 when SW(0) = '1' and SW(1) = '1' and SW(2) = '1' and SW(3) = '0' else
+		n8 when SW(0) = '0' and SW(1) = '0' and SW(2) = '0' and SW(3) = '1' else
+		n9 when SW(0) = '1' and SW(1) = '0' and SW(2) = '0' and SW(3) = '1' else
+		a when SW(0) = '0' and SW(1) = '1' and SW(2) = '0' and SW(3) = '1' else
+		b when SW(0) = '1' and SW(1) = '1' and SW(2) = '0' and SW(3) = '1' else
+		c when SW(0) = '0' and SW(1) = '0' and SW(2) = '1' and SW(3) = '1' else
+		d when SW(0) = '1' and SW(1) = '0' and SW(2) = '1' and SW(3) = '1' else
+		e when SW(0) = '0' and SW(1) = '1' and SW(2) = '1' and SW(3) = '1' else
+		f when SW(0) = '1' and SW(1) = '1' and SW(2) = '1' and SW(3) = '1';
+		
+	HEX1 <= 
+		n0 when SW(4) = '0' and SW(5) = '0' and SW(6) = '0' and SW(7) = '0' else
+		n1 when SW(4) = '1' and SW(5) = '0' and SW(6) = '0' and SW(7) = '0' else
+		n2 when SW(4) = '0' and SW(5) = '1' and SW(6) = '0' and SW(7) = '0' else
+		n3 when SW(4) = '1' and SW(5) = '1' and SW(6) = '0' and SW(7) = '0' else
+		n4 when SW(4) = '0' and SW(5) = '0' and SW(6) = '1' and SW(7) = '0' else
+		n5 when SW(4) = '1' and SW(5) = '0' and SW(6) = '1' and SW(7) = '0' else
+		n6 when SW(4) = '0' and SW(5) = '1' and SW(6) = '1' and SW(7) = '0' else
+		n7 when SW(4) = '1' and SW(5) = '1' and SW(6) = '1' and SW(7) = '0' else
+		n8 when SW(4) = '0' and SW(5) = '0' and SW(6) = '0' and SW(7) = '1' else
+		n9 when SW(4) = '1' and SW(5) = '0' and SW(6) = '0' and SW(7) = '1' else
+		a when SW(4) = '0' and SW(5) = '1' and SW(6) = '0' and SW(7) = '1' else
+		b when SW(4) = '1' and SW(5) = '1' and SW(6) = '0' and SW(7) = '1' else
+		c when SW(4) = '0' and SW(5) = '0' and SW(6) = '1' and SW(7) = '1' else
+		d when SW(4) = '1' and SW(5) = '0' and SW(6) = '1' and SW(7) = '1' else
+		e when SW(4) = '0' and SW(5) = '1' and SW(6) = '1' and SW(7) = '1' else
+		f when SW(4) = '1' and SW(5) = '1' and SW(6) = '1' and SW(7) = '1';
+		
+	HEX2 <= 
+		n0 when SW(8) = '0' and SW(9) = '0'else
+		n1 when SW(8) = '1' and SW(9) = '0'else
+		n2 when SW(8) = '0' and SW(9) = '1'else
+		n3 when SW(8) = '1' and SW(9) = '1';
+
 end rtl;
