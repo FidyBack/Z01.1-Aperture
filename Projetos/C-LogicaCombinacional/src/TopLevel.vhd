d67029bb6db3954fe6e16f5ff48a68d2832775a6
phdaher <pedrohenriquedaher@me.com>
2019-03-15 07:58:12 -0300

    Correcoes

--- Projetos/C-LogicaCombinacional/src/TopLevel.vhd
+++ Projetos/C-LogicaCombinacional/src/TopLevel.vhd
@@ -24,12 +24,8 @@ entity TopLevel is
 		HEX1    : out std_logic_vector(6 downto 0); -- 7seg1
 		HEX2    : out std_logic_vector(6 downto 0); -- 7seg2
 		HEX3    : out std_logic_vector(6 downto 0); -- 7seg3
-<<<<<<< HEAD
 		HEX4    : out std_logic_vector(6 downto 0); -- 7seg4
 		HEX5    : out std_logic_vector(6 downto 0); -- 7seg5
-=======
->>>>>>> f68b0efcb8eaeca55c8d883febb9af1b3d716556
-		LEDR    : out std_logic_vector(9 downto 0)
 	);
 end entity;
 
@@ -47,28 +43,7 @@ architecture rtl of TopLevel is
 ---------------
 		  
 begin
-<<<<<<< HEAD
-  when SW(0) = '0'
-   HEX(0) <= '0';
-	HEX(1) <= '0';
-	HEX(2) <= '0';
-	HEX(3) <= '0';
-	HEX(4) <= '0';
-	HEX(5) <= '0';
-	HEX(6) <= '1';
-  else
-	HEX(0) <= '1';
-	HEX(1) <= '0';
-	HEX(2) <= '0';
-	HEX(3) <= '1';
-	HEX(4) <= '1';
-	HEX(5) <= '1';
-	HEX(6) <= '1';
-
-
-=======
-	HEX0(1) <=SW(0);
-	HEX0(4) <=SW(0);
-	LEDR(0) <= SW(0);
->>>>>>> f68b0efcb8eaeca55c8d883febb9af1b3d716556
+	with SW(0) select
+		HEX0 <= "0000001" when '0',
+			"1001111" when others;
 end rtl;
