0fb1e9222c358a1fb30ee4d9f90c136983071db9
luvi01 <luvi@germanos.ws>
2019-05-02 13:32:05 -0300

    Control Unit + CPU

--- Projetos/G-Computador/src/rtl/ControlUnit.vhd
+++ Projetos/G-Computador/src/rtl/ControlUnit.vhd
@@ -15,14 +15,34 @@ entity ControlUnit is
 		muxALUI_A                   : out STD_LOGIC;                     -- mux que seleciona entre instrução e ALU para reg. A
 		muxAM                       : out STD_LOGIC;                     -- mux que seleciona entre reg. A e Mem. RAM para ALU
 		muxSD_ALU                   : out STD_LOGIC;                     -- mux que seleciona entre reg. S e reg. D
-	   muxAMD_ALU                  : out STD_LOGIC;                     -- mux que seleciona entre reg. A e Mem. RAM para ALU
+	   	muxAMD_ALU                  : out STD_LOGIC;                     -- mux que seleciona entre reg. A e Mem. RAM para ALU
 		zx, nx, zy, ny, f, no       : out STD_LOGIC;                     -- sinais de controle da ALU
-	   loadA, loadD, loadS, loadM, loadPC : out STD_LOGIC               -- sinais de load do reg. A, reg. D,
+	    loadA, loadD, loadS, loadM, loadPC : out STD_LOGIC               -- sinais de load do reg. A, reg. D,
          	                                                           -- Mem. RAM e Program Counter
     );
 end entity;
 
 architecture arch of ControlUnit is
 begin
+
+loadS <= instruction(17) and instruction(5);
+loadD <= instruction(17) and instruction(4);
+loadM <= instruction(17) and instruction(3);
+loadA <= (instruction(17) and instruction(6)) or NOT(instruction(17));
+
+
+
+zx <= instruction(17) and instruction(12);
+nx <= instruction(17) and instruction(11);
+zy <= instruction(17) and instruction(10);
+ny <= instruction(17) and instruction(9);
+f <= instruction(17) and instruction(8);
+no <= instruction(17) and instruction(7);
+
+muxALUI_A <= not instruction(17);
+muxAMD_ALU <= instruction(17) and NOT(instruction(15));
+muxSD_ALU <= instruction(17) and instruction(13);
+muxAM <= instruction(14) and instruction(17);
+loadPC <= instruction(17) and ((instruction(2) and ng) or (instruction(1) and zr) or (instruction(0) and NOT(ng) and NOT(zr)));
 
 end architecture;
